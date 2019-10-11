02dc7dbdb2a0b17cbb4956322440a804de326070
Yunjin Xu <xuyunin@foxmail.com>
2019-10-11 17:38:00 +0800

    初步完成，endpoint有待配置

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/wooln
 
 go 1.12
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway v1.11.3 // indirect
+	github.com/kardianos/service v1.0.0 // indirect
+	google.golang.org/grpc v1.24.0 // indirect
+)
--- src/proxy-app.go
+++ src/proxy-app.go
@@ -1,10 +1,22 @@
 package main
 
-import (
-	"log"
+import (	
 	proxyLib "github.com/wooln/seagull2-grpc-webapi-proxy"
+	"log"
+	gw "Foo_Contracts"  // Update
 )
 
 func main()  {
+	actions := []proxyLib.RegisterAction {
+		proxyLib.RegisterAction{
+			gw.RegisterGreeterHandlerFromEndpoint,
+			"localhost:8080",
+		},		
+		proxyLib.RegisterAction{
+			gw.RegisterGreeterNewHandlerFromEndpoint,
+			"localhost:8080",
+		},		
+	}
 	log.Printf(proxyLib.Msg)
+	proxyLib.Proxy(actions)
 }
